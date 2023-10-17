b922ca2abddc080560451cf4e59a2f73665b751b
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2023-10-17 15:48:59 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,34 +2,37 @@ public class Main {
     public static void main(String[] args) {
 
         // METHOD OVERLOADING ........ //
-        // Pizza pizza = new Pizza("thicc crust", "tomato", "mozzarella", "pepperoni");
-        // Pizza pizza = new Pizza("thicc crust", "tomato", "mozzarella");
-        // Pizza pizza = new Pizza("thicc crust", "tomato");
-        // Pizza pizza = new Pizza("thicc crust");
-        // Pizza pizza = new Pizza();
+        Pizza pizza = new Pizza("thicc crust", "tomato", "mozzarella", "pepperoni");
+        Pizza pizza = new Pizza("thicc crust", "tomato", "mozzarella");
+        Pizza pizza = new Pizza("thicc crust", "tomato");
+        Pizza pizza = new Pizza("thicc crust");
+        Pizza pizza = new Pizza();
         
-        // System.out.println("Here are your pizza ingredients: ");
-        // System.out.println(pizza.bread);
-        // System.out.println(pizza.sauce);
-        // System.out.println(pizza.cheese);
-        // System.out.println(pizza.topping);
+        System.out.println("Here are your pizza ingredients: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+
+
+        // ARRAY OF OBJECTS ........... //
+        Food[] refrigerator = new Food[3];
         
-        // Food[] refrigerator = new Food[3];
+        Food food1 = new Food("Rice");
+        Food food2 = new Food("Banku");
+        Food food3 = new Food("Fufu");
         
-        // Food food1 = new Food("Rice");
-        // Food food2 = new Food("Banku");
-        // Food food3 = new Food("Fufu");
-        
-        // Food[] refrigerator = {food1, food2, food3};
+        Food[] refrigerator = {food1, food2, food3};
         
         // refrigerator[0] = food1;
         // refrigerator[1] = food2;
         // refrigerator[2] = food3;
         
-        // for (int i = 0; i < refrigerator.length; i++) {
-        //       System.out.println(refrigerator[i].name);
-        // } 
-        
+        for (int i = 0; i < refrigerator.length; i++) {
+              System.out.println(refrigerator[i].name);
+        } 
+
+        // PASS OBJECT TO ANOTHER OBJECT
         Car car = new Car("BMW");
         Garage garage = new Garage();
         
