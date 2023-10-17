71ddce4d26b7a5dddfb7809fc49ad2cda4cf294d
Bernard Ograh <69661398+bograh@users.noreply.github.com>
2023-10-17 15:44:10 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        // Pizza pizza = new Pizza("thicc crust", "tomato", "mozzarella", "pepperoni");
+        
+        // System.out.println("Here are your pizza ingredients: ");
+        // System.out.println(pizza.bread);
+        // System.out.println(pizza.sauce);
+        // System.out.println(pizza.cheese);
+        // System.out.println(pizza.topping);
+        
+        // Food[] refrigerator = new Food[3];
+        
+        // Food food1 = new Food("Rice");
+        // Food food2 = new Food("Banku");
+        // Food food3 = new Food("Fufu");
+        
+        // Food[] refrigerator = {food1, food2, food3};
+        
+        // refrigerator[0] = food1;
+        // refrigerator[1] = food2;
+        // refrigerator[2] = food3;
+        
+        // for (int i = 0; i < refrigerator.length; i++) {
+        //       System.out.println(refrigerator[i].name);
+        // } 
+        
+        Car car = new Car("BMW");
+        Garage garage = new Garage();
+        
+        garage.park(car);
+        
+    }
+}
+
